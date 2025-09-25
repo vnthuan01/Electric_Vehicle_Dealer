2d7ff03ab1ace01de221e7c06e8103c31084aa93
Nguyễn Thuận <vnthuan02@gmail.com>
2025-09-25 17:16:58 +0700

    refactor(order): fix flow order

--- src/models/Order.js
+++ src/models/Order.js
@@ -3,13 +3,16 @@ import mongoose from "mongoose";
 const orderSchema = new mongoose.Schema(
   {
     code: {type: String, required: true, unique: true, index: true},
+
     customer_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Customer",
       required: true,
     },
+
     dealership_id: {type: mongoose.Schema.Types.ObjectId, ref: "Dealership"},
     salesperson_id: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+
     items: [
       {
         vehicle_id: {
@@ -17,26 +20,62 @@ const orderSchema = new mongoose.Schema(
           ref: "Vehicle",
           required: true,
         },
+
+        // --- snapshot vehicle ---
+        vehicle_name: String,
+        vehicle_price: Number,
+
         quantity: {type: Number, min: 1, max: 100, default: 1},
-        price: {type: Number, required: true},
         discount: {type: Number, default: 0},
         promotion_id: {type: mongoose.Schema.Types.ObjectId, ref: "Promotion"},
+
+        // --- snapshot accessories ---
+        accessories: [
+          {
+            accessory_id: {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "Accessory",
+              required: true,
+            },
+            name: String,
+            price: Number,
+            quantity: {type: Number, default: 1},
+          },
+        ],
+
+        // --- snapshot options ---
+        options: [
+          {
+            option_id: {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "Option",
+              required: true,
+            },
+            name: String,
+            price: Number,
+          },
+        ],
+
         final_amount: {type: Number, required: true},
       },
     ],
+
     final_amount: {type: Number, required: true},
     paid_amount: {type: Number, default: 0},
+
     payment_method: {
       type: String,
       enum: ["cash", "paypal", "zalopay", "installment"],
       default: "cash",
     },
+
     status: {
       type: String,
       enum: ["quote", "confirmed", "contract_signed", "delivered"],
       default: "quote",
       index: true,
     },
+
     notes: String,
   },
   {timestamps: true}
