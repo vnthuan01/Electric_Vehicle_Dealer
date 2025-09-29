1be8f71554860fdfeeab9f8529957e1e01361681
Nguyễn Thuận <vnthuan02@gmail.com>
2025-09-29 14:27:44 +0700

    refactor(cors): ok

--- src/app.js
+++ src/app.js
@@ -32,11 +32,7 @@ dotenv.config();
 const app = express();
 
 // --- Middleware ---
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors({}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(httpLogger);
